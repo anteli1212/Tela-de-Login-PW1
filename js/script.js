09604f589b5d373f3b1636ed323662e86013b1b9
guilherme <guilherme.dotelilima@gmail.com>
2025-05-13 19:14:49 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function Logar(event){
+    event.preventDefault();
+    const email = document.getElementById("inputEmail").value;
+    const pas = document.getElementById("inputSenha").value;
+
+    console.log(`Email: ${email} e Senha: ${pas}`);
+
+    if (email == "KanyeWest@Yahoo.com" && pas == "ImEvil"){
+        alert(`Usuário ${email} logado com sucesso!`);
+        window.location.href = "pag2.html";
+    }else{
+        alert("Usuário não cadastrado :/, revise seus dados");
+    }
+}
